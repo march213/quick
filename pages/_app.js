813c9a6f74867a3fc2fe21247ce548d528f2238a
Eugenia Molodetskaya <jane.molodetskaya@gmail.com>
2020-09-05 20:02:49 -0400

    basic firebase + github auth (#1)

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
-import React from "react"
+import React from 'react'
 import '../styles/globals.css'
+import { ProviderAuth } from '../lib/auth'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ProviderAuth>
+      <Component {...pageProps} />
+    </ProviderAuth>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { useAuth } from '../lib/auth'
 
 export default function Home() {
+  const auth = useAuth()
   return (
     <div className={styles.container}>
       <Head>
         <title>Quick Feedback</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section>super fast</section>
+      <section>
+        <h1>Quick Feedback</h1>
+        <p>{auth?.user?.displayName}</p>
+        {auth?.user ? (
+          <button onClick={() => auth.signout()}>Sign out</button>
+        ) : (
+          <button onClick={() => auth.signin()}>Sign in</button>
+        )}
+      </section>
     </div>
   )
 }
